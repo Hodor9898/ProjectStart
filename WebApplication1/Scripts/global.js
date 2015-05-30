4936e74980e5c3d7ccaa084955ce43d2a214b010
smurf <mbytester9898@gmail.com>
2015-05-30 16:36:11 +0300

    asd

---
+++ WebApplication1/Scripts/global.js
@@ -0,0 +1,25 @@
+﻿var moduleApp = angular.module('mainApp', []);
+
+moduleApp.controller('HomeController',['$scope',function($scope) {
+    
+}]);
+
+moduleApp.controller('ProjectController',['$scope',function($scope) {
+    
+}]);
+
+moduleApp.config(['$routeProvider',
+  function($routeProvider) {
+      $routeProvider.
+        when('/project/:projectId', {
+            templateUrl: 'partials/project.html',
+            controller: 'ProjectController'
+        }).
+        when('/home', {
+            templateUrl: 'partials/home.html',
+            controller: 'HomeController'
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
+  }]);
